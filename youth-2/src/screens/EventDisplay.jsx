1d400db02e15abd201e5905801a344d1b24974e0
guess01325 <guess01325@gmail.com>
2023-08-12 14:56:50 -0400

    changed event display

--- youth-2/src/screens/EventDisplay.jsx
+++ youth-2/src/screens/EventDisplay.jsx
@@ -1,5 +1,5 @@
-import { Outlet, useOutletContext,Link } from "react-router-dom";
-import "../assets/Events.css"
+import { Outlet, useOutletContext, Link } from "react-router-dom";
+import "../assets/Events.css";
 
 import {
   Toolbar,
@@ -18,60 +18,53 @@ import {
   FormControl,
 } from "@mui/material";
 
-
-
 export default function EventDisplay(props) {
   const theme = createTheme();
-  const [events, setEvents, handleDeleteEvent, handleUpdateEvent, handleCreateEvent] = useOutletContext();
-  
-  
-  
-  
-  
-  
+  const [
+    events,
+    setEvents,
+    handleDeleteEvent,
+    handleUpdateEvent,
+    handleCreateEvent,
+  ] = useOutletContext();
+
   return (
     <ThemeProvider theme={theme}>
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-    <Box
-    sx={{
-      mt: 14,
-      mb: 7,
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-
-    }}
-    >
-
-
-<Box
-sx={{
-  margin: 1,
-  width: "200px",
-  alignItems: "center",
-  justifyContent: "center",
-  border: 3,
-  borderRadius: 6,
-  fontSize: "20px"
-}}>
-{events.map((event) => (
-        <div className="event-display" key={events.id}>
-
-  <p> {event.name}</p>
-  <p>{event.place}</p>
-  <p> {event.date}</p>
-  <p>{event.time}</p>
-  </div>
-  
-  ))}
-      <Link to>RSVP</Link>
-
-</Box>
-    
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            mt: 14,
+            mb: 7,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              margin: 1,
+              width: "200px",
+              alignItems: "center",
+              justifyContent: "center",
+              border: 3,
+              borderRadius: 6,
+              fontSize: "20px",
+            }}
+          >
+            {events.map((event) => (
+              <div className="event-display" key={events.id}>
+                <p> {event.name}</p>
+                <p>{event.place}</p>
+                <p> {event.date}</p>
+                <p>{event.time}</p>
+              </div>
+            ))}
+            <Link to>RSVP</Link>
           </Box>
-          </Container>
+        </Box>
+      </Container>
     </ThemeProvider>
   );
 }
