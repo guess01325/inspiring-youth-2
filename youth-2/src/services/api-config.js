23acff97ec26eabf57f5a0226f34b240c49df454
guess01325 <guess01325@gmail.com>
2023-08-12 14:33:17 -0400

    changed server url to new heroku app

--- youth-2/src/services/api-config.js
+++ youth-2/src/services/api-config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const baseUrl = 
 process.env.NODE_ENV === "production" 
- ? "https://inspiring-7f054c73d0ae.herokuapp.com/" : "http://localhost:3000" 
+ ? "/home/otis/General-Assembly/inspiring-youth/client" : "http://localhost:3000" 
 
  const api = axios.create({
   baseURL: baseUrl
